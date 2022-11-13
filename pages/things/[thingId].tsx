ccc107aa09ce5f69cc369d454b6c4926bef0fbae
Filipe Dobreira <dobreira@gmail.com>
2022-11-13 22:36:56 +0000

    delete unused thing page

--- pages/things/[thingId].tsx
+++
@@ -1,9 +0,0 @@
-import { useThing } from '@lib/things/useThing';
-
-const ThingPage = () => {
-  const { thing } = useThing({ id: 3 });
-
-  return <div>{thing?.name}</div>;
-};
-
-export default ThingPage;
